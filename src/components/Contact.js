4bf4c82b51834c9595daba2d13f1d982197ba88f
Ignacio De Maio <ignacio.demaio@nzx.com>
2018-08-08 16:28:59 +1200

    Add detail mode to contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import classnames from 'classnames'
 
 export default class Contact extends Component {
   static propTypes = {
     contact: PropTypes.object.isRequired,
+    detailMode: PropTypes.bool.isRequired,
     isSelected: PropTypes.bool.isRequired,
     onEditContactClick: PropTypes.func.isRequired, // TODO Instead of handle this, should try router
     // deleteContact: PropTypes.func.isRequired
@@ -16,13 +18,36 @@ export default class Contact extends Component {
     this.props.onEditContactClick(this.props.contact)
   };
 
-  state = {}
+  state = {};
 
   render() {
-    const { contact, isSelected } = this.props
+    const { contact, isSelected, detailMode } = this.props;
 
-    return (
-      <li className={`media col-md-6 col-lg-4 ${isSelected ? 'selected' : ''}`} onClick={this.handleEditClick}>
+    let classNames = classnames('media', {
+      'col-md-6 col-lg-4': !detailMode,
+      'col-sm-12': detailMode,
+      selected: isSelected,
+      detail: detailMode
+      })
+
+    let element;
+
+    if (detailMode) {
+      element = (
+        <div className="row contact">
+          <div className="col-sm-2 contact-avatar">
+            <img className="media-object" src={`${contact.avatarUrl}`}/>
+          </div>
+          <div className="col-sm-10 contact-description">
+            <p className="contact-fullname">
+              {contact.firstName} {contact.lastName}
+            </p>
+          </div>
+        </div>
+      )
+    } else {
+      element = (
+        <div>
           <div className="thumbnail">
             <img className="media-object" src={`${contact.avatarUrl}`}/>
           </div>
@@ -37,7 +62,13 @@ export default class Contact extends Component {
             </dl>
           </div>
           <hr/>
+        </div>
+      )
+    }
 
+    return (
+       <li className={classNames}  onClick={this.handleEditClick}>
+        {element}
       </li>
     )
   }
