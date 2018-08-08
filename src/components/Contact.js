deaaa2316ada7f89b31da5413818a0081ec50349
Ignacio De Maio <ignacio.demaio@nzx.com>
2018-08-08 14:46:39 +1200

    Add background color to contact when selected
    Removing delete buttong and phone number

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,27 +22,17 @@ export default class Contact extends Component {
     const { contact, isSelected } = this.props
 
     return (
-      <li className="media col-md-6 col-lg-4" onClick={this.handleEditClick}>
-          {isSelected ? 'SELECTED' : 'NO'}
+      <li className={`media col-md-6 col-lg-4 ${isSelected ? 'selected' : ''}`} onClick={this.handleEditClick}>
           <div className="thumbnail">
             <img className="media-object" src={`${contact.avatarUrl}`}/>
           </div>
           <div className="media-heading">
             <h3>
               {contact.firstName} {contact.lastName}
-              <small>
-
-                <a href="#contacts/delete/{contact.id}" className="delete-contract">
-                  <span className="glyphicon glyphicon-trash"></span>
-                </a>
-              </small>
             </h3>
           </div>
           <div className="media-body">
             <dl>
-              <dt>Phone Number:</dt>
-              <dd>{contact.tel}</dd>
-              <dt>Email:</dt>
               <dd>{contact.email}</dd>
             </dl>
           </div>
