a3199ab747a7725bac7058f6484df5c0ae374cd0
Ignacio De Maio <ignacio.demaio@nzx.com>
2018-08-08 13:37:19 +1200

    Handle edit click on contact card, removing edit button

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,33 +22,32 @@ export default class Contact extends Component {
     const { contact, isSelected } = this.props
 
     return (
-      <li className="media col-md-6 col-lg-4">
-        {isSelected ? 'SELECTED' : 'NO'}
-        <div className="thumbnail">
-          <img className="media-object" src={`${contact.avatarUrl}`}/>
-        </div>
-        <div className="media-heading">
-          <h3>
-            {contact.firstName} {contact.lastName}
-            <small>
-              <a href={`contacts/edit/${contact.id}`} onClick={this.handleEditClick}>
-                <span className="glyphicon glyphicon-pencil"></span>
-              </a>
-              <a href="#contacts/delete/{contact.id}" className="delete-contract">
-                <span className="glyphicon glyphicon-trash"></span>
-              </a>
-            </small>
-          </h3>
-        </div>
-        <div className="media-body">
-          <dl>
-            <dt>Phone Number:</dt>
-            <dd>{contact.tel}</dd>
-            <dt>Email:</dt>
-            <dd>{contact.email}</dd>
-          </dl>
-        </div>
-        <hr/>
+      <li className="media col-md-6 col-lg-4" onClick={this.handleEditClick}>
+          {isSelected ? 'SELECTED' : 'NO'}
+          <div className="thumbnail">
+            <img className="media-object" src={`${contact.avatarUrl}`}/>
+          </div>
+          <div className="media-heading">
+            <h3>
+              {contact.firstName} {contact.lastName}
+              <small>
+
+                <a href="#contacts/delete/{contact.id}" className="delete-contract">
+                  <span className="glyphicon glyphicon-trash"></span>
+                </a>
+              </small>
+            </h3>
+          </div>
+          <div className="media-body">
+            <dl>
+              <dt>Phone Number:</dt>
+              <dd>{contact.tel}</dd>
+              <dt>Email:</dt>
+              <dd>{contact.email}</dd>
+            </dl>
+          </div>
+          <hr/>
+
       </li>
     )
   }
