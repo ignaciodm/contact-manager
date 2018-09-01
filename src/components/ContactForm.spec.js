44da5b15af2bddc3c5d5fce07d64592bbdae0437
Ignacio De Maio <ignacio.demaio@nzx.com>
2018-09-01 14:08:23 +1200

    Refactor splitting test

--- src/components/ContactForm.spec.js
+++ src/components/ContactForm.spec.js
@@ -48,10 +48,22 @@ const setup = ({isEditing = true}) => {
 
   let output = renderer.getRenderOutput()
 
+  const [cardHeader, cardBody] = output.props.children
+  const [firstNameGroup, lastNameGroup, emailGroup, phoneGroup, buttonGroups] = cardBody.props.children
+  const [primaryButton] = buttonGroups.props.children.props.children
+
   return {
     props: props,
     output: output,
-    renderer: renderer
+    renderer: renderer,
+    cardHeader: cardHeader,
+    cardBody: cardBody,
+    firstNameGroup: firstNameGroup,
+    lastNameGroup: lastNameGroup,
+    emailGroup: emailGroup,
+    phoneGroup: phoneGroup,
+    buttonGroups: buttonGroups,
+    primaryButton: primaryButton
   }
 }
 
@@ -64,69 +76,76 @@ describe('ContactForm component', () => {
     StyleSheetTestUtils.clearBufferAndResumeStyleInjection()
   })
 
-  describe('Edit form', () => {
-    it('edit initial render', () => {
-      const { output } = setup({})
+  describe('render', () => {
+    it('should renders a div', () => {
+      expect(setup({}).output.type).toBe('div')
+    })
 
-      expect(output.type).toBe('div')
-      expect(output.props.className).toEqual('contact-form card row')
+    it('should renders proper classes', () => {
+      expect(setup({}).output.props.className).toEqual(expect.stringContaining('contact-form card row form'))
+    })
+  })
 
-      const [cardHeader, cardBody] = output.props.children
-      expect(cardHeader.type).toBe('h4')
-      expect(getTextContent(cardHeader)).toBe('Edit Contact')
+  describe('edit form mode', () => {
+    it('should sets header title', () => {
+      expect(getTextContent(setup({}).cardHeader)).toBe('Edit Contact')
+    })
 
+    it('should set a form as body' , () => {
+      const { cardBody } = setup({})
       expect(cardBody.type).toBe('form')
+    })
 
-      const [firstNameGroup, lastNameGroup, emailGroup, phoneGroup, buttonGroups] = cardBody.props.children
-
-      const [primaryButton] = buttonGroups.props.children.props.children
+    it('should set input values', () => {
+      const { firstNameGroup, lastNameGroup, emailGroup, phoneGroup} = setup({})
 
       expect(firstNameGroup.props.value).toBe(contact.firstName)
       expect(lastNameGroup.props.value).toBe(contact.lastName)
       expect(emailGroup.props.value).toBe(contact.email)
       expect(phoneGroup.props.value).toBe(contact.tel)
+    })
+
+    it('sets submit button text', () => {
+      const {primaryButton} = setup({})
       expect(getTextContent(primaryButton)).toBe('Update Contact')
     })
   })
 
-  describe('Add new form', () => {
-    it('initial render', () => {
-      const { output } = setup({ isEditing: false })
-
-      expect(output.type).toBe('div')
-      expect(output.props.className).toEqual('contact-form card row')
-
-      const [cardHeader, cardBody] = output.props.children
-      expect(cardHeader.type).toBe('h4')
+  describe('add new form', () => {
+    it('should sets header title', () => {
+      const { cardHeader } =  setup({ isEditing: false })
       expect(getTextContent(cardHeader)).toBe('Add Contact')
+    })
 
-      expect(cardBody.type).toBe('form')
-
-      const [firstNameGroup, lastNameGroup, emailGroup, phoneGroup, buttonGroups] = cardBody.props.children
-      const [primaryButton] = buttonGroups.props.children.props.children
+    it('should set input values', () => {
+      const { firstNameGroup, lastNameGroup, emailGroup, phoneGroup} = setup({ isEditing: false })
 
       expect(firstNameGroup.props.value).toBe(newContact.firstName)
       expect(lastNameGroup.props.value).toBe(newContact.lastName)
       expect(emailGroup.props.value).toBe(newContact.email)
       expect(phoneGroup.props.value).toBe(newContact.tel)
+    })
+
+    it('sets submit button text', () => {
+      const {primaryButton} = setup({ isEditing: false })
       expect(getTextContent(primaryButton)).toBe('Add Contact')
     })
   })
 
   it('onSubmit should call handleSubmit with contact', () => {
     const { output, props } = setup({})
-
     const [, cardBody] = output.props.children
+
     cardBody.props.onSubmit({preventDefault: jest.fn()})
     expect(props.onSubmit).toBeCalledWith(contact)
   })
 
   it('on cancel click should call onCancel', () => {
     const { output, props } = setup({})
-
     const [, cardBody] = output.props.children
     const [, , , , buttonGroups] = cardBody.props.children
     const [, cancelButton] = buttonGroups.props.children.props.children
+
     cancelButton.props.onClick({preventDefault: jest.fn()})
     expect(props.onCancel).toBeCalled()
   })
